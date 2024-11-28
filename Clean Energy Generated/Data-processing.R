67f00bb43f334a3601299e19691434d9662dfb80
Naji <Mehdi.Naji@gov.bc.ca>
2024-11-28 14:46:38 -0800

    Update Datasets

--- Clean Energy Generated/Data-processing.R
+++ Clean Energy Generated/Data-processing.R
@@ -13,6 +13,11 @@ df1$DATE <- ymd(df1$REF_DATE)
 df1$Year <- year(df1$DATE)
 df1$Month <- month(df1$DATE)
 
+max <- df1 |>
+  filter (Year == max(Year)) |>
+  group_by(Year) |>
+  summarise(max = max(Month))
+
 df2$DATE <- ymd(df2$REF_DATE)
 df2$Year <- year(df2$DATE)
 df2$Month <- month(df2$DATE)
@@ -23,7 +28,8 @@ df2 <- df2 |> select(DATE, Year, Month, GEO, `Electric power, components`, VALUE
 
 df1_1 <- df1 |> 
   group_by(Year, GEO, `Class of electricity producer`, `Type of electricity generation`, UOM) |>
-  summarise(VALUE = sum(VALUE))
+  summarise(VALUE = sum(VALUE)) |>
+  mutate (lastmonth = max$max)
 
 
 # df2_1 <- df2 |>
